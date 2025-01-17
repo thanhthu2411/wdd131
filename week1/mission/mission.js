524edf30168279b9d391c4a0cd16e31d7afe5294
thanhthu2411 <thu24@byui.edu>
2025-01-17 13:12:45 -0700

    mission-js

--- week1/mission/mission.js
+++ week1/mission/mission.js
@@ -1,12 +1,22 @@
+
+const themeSelector = document.querySelector('#theme');
+
+// replace with code to select dropdown element out of the HTML (Hint: document.querySelector)
+
 function changeTheme() {
-    if (document.querySelector("#theme").value === "dark") {
-        document.querySelector("body").classList.add("dark") 
-        document.querySelector("img").src = "byui-logo_white.png"
+// check to see what the current value of our select is.
+// The current value is conveniently found in themeSelector.value!
+    console.log(themeSelector.value);
+
+    if (themeSelector.value === 'dark') {
+        document.querySelector('body').classList.add('dark');
+        document.querySelector('img').src = 'byui-logo_white.png';
     } else {
-        document.querySelector("body").classList.remove("dark")
-        document.querySelector("img").src = "byui-logo_blue.webp"
+        document.querySelector('body').classList.remove('dark');
+        document.querySelector('img').src = 'byui-logo_blue.webp';
     }
 }
 
-theme = document.getElementById("theme").addEventListener("change", changeTheme);
-
+// add an event listener to the themeSelector element here.
+// Use the changeTheme function as the event handler function.
+themeSelector.addEventListener('change', changeTheme);
